e474323689dae0b1852eaa232c2038abd7992538
Aman Kant <amantheiit@gmail.com>
2023-09-24 15:20:21 +0530

    Intial Commit

---
+++ Assets/Scripts/Saw.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+
+    public float speed = 2;
+    int dir = 1;
+
+    public Transform rightCheck;
+    public Transform leftCheck;   
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector2.right * speed * dir * Time.deltaTime);
+        if (Physics2D.Raycast(rightCheck.position, Vector2.down,2) == false)
+        {
+            dir = -1;
+        }
+        if (Physics2D.Raycast(leftCheck.position, Vector2.down, 2) == false)
+        {
+            dir = 1;
+        }
+
+    }
+}
